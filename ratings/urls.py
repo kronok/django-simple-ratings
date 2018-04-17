5a1c63ae720a0e24107561a7ad71477689cc94f4
kronok <brandonjur@gmail.com>
2018-04-17 01:26:02 -0400

    django 2.0 compat

--- ratings/urls.py
+++ ratings/urls.py
@@ -1,11 +1,7 @@
-try:
-    # Django >= 1.6
-    from django.conf.urls import patterns, url
-except ImportError:  # pragma: no cover
-    # Django < 1.6
-    from django.conf.urls.defaults import patterns, url
+from django.urls import re_path
 
-urlpatterns = patterns('ratings.views',
-    url(r'^rate/(?P<ct>\d+)/(?P<pk>[^\/]+)/(?P<score>\-?[\d\.]+)/$', 'rate_object', name='ratings_rate_object'),
-    url(r'^unrate/(?P<ct>\d+)/(?P<pk>[^\/]+)/$', 'rate_object', {'add': False}, name='ratings_unrate_object'),
-)
+
+urlpatterns = ['ratings.views',
+    re_path(r'^rate/(?P<ct>\d+)/(?P<pk>[^\/]+)/(?P<score>\-?[\d\.]+)/$', 'rate_object', name='ratings_rate_object'),
+    re_path(r'^unrate/(?P<ct>\d+)/(?P<pk>[^\/]+)/$', 'rate_object', {'add': False}, name='ratings_unrate_object'),
+]
