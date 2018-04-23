53b376c9e4595d2fb6f9dea84edb18e578aab64a
kronok <brandonjur@gmail.com>
2018-04-23 17:14:12 -0400

    django 2.0 support

--- ratings/utils.py
+++ ratings/utils.py
@@ -15,7 +15,7 @@ from django.db import connection
 
 def get_content_object_field(rating_model):
     opts = rating_model._meta
-    for virtual_field in opts.virtual_fields:
+    for virtual_field in opts.private_fields:
         if virtual_field.name == 'content_object':
             return virtual_field  # break out early
     return opts.get_field('content_object')
